f2b879e1714d94419fa9c1f506e4a5f3782c8ef1
rajnish432 <rajnish.kahar1996@gmail.com>
2020-08-08 09:19:49 +0530

    ADDED: Welcome To Census Analyser

---
+++ CensusAnalyser/CensusAnalyser.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CensusAnalyser
+{
+    public class CensusAnalyser
+    {
+    }
+}
---
+++ CensusAnalyserTest/CensusAnalyserTest.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace CensusAnalyserTest
+{
+    public class CensusAnalyserTest
+    { 
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void test1()
+        {
+        }
+    }
+}
